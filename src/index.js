b0c09c198b1f0e33fbc1f81539e223eeb93b6c18
ndhanh96 <ndhanh96@gmail.com>
2021-09-30 13:36:30 +0700

    fk this route

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ app.use(
 );
 
 app.use(passport.initialize());
-app.use(passport.authenticate('session'));
+app.use(passport.authenticate('session',{session:true}));
 
 app.get('/', (req, res) => {
   res.send(`Hello ${req.user ? req.user.username : 'World'}`);
